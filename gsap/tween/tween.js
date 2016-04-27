d35b5b1a31d087f4c160d8e8a9fab1cf92dd3467
LD <ld_1989_pvl@yahoo.com>
2016-04-27 23:54:31 +0100

    rolling squares and flying brick

--- gsap/tween/tween.js
+++ gsap/tween/tween.js
@@ -17,13 +17,49 @@
 
         console.log('The DOM is ready');
 
-        var dot = $('.dot');
+        var tl = new TimelineLite();
 
-        TweenLite.to(dot, 2, {
-            y: "80%",
+        var circle = $('.circle');
+        var squares = $('.square');
+        var bar = $('.bar');
+
+        TweenLite.to(circle, 2, {
+            y: "100%",
             ease: Power1.easeInOut,
             delay: 1
         });
+
+        tl.staggerFrom(squares, 2, {
+                x: -500,
+                rotation: "-=600",
+                ease: Power1.easeInOut,
+                autoAlpha: -3 // fade in
+            }, 0.1)
+            .fromTo(bar, 5, {
+                y: -100
+            }, {
+                bezier: {
+                    type: "soft",
+                    values: [{
+                        x: 150,
+                        y: 300
+                    }, {
+                        x: 200,
+                        y: 100
+                    }, {
+                        x: 260,
+                        y: 300
+                    }, {
+                        x: 330,
+                        y: 140
+                    }],
+                    autoRotate: [["x", "y", "rotation", 0, false],
+                    ["x", "y", "rotation", 90, false],
+                    ["x", "y", "rotation", -180, false],
+                    ["x", "y", "rotation", 0, false]]
+                },
+                ease: Power1.easeOut
+            });
 
     });
 
