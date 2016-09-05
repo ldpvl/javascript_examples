cea080b2212ae86a1b984948ec8d4113e2873db9
LD <ld_1989_pvl@yahoo.com>
2016-09-05 22:01:47 +0100

    added foundation sticky navbar example

---
+++ foundation/sticky-navbar/app.js
@@ -0,0 +1,30 @@
+// http://gregfranko.com/jquery-best-practices
+
+// IIFE - Immediately Invoked Function Expression
+// http://benalman.com/news/2010/11/immediately-invoked-function-expression/#iife
+(function (f) {
+
+    // The global jQuery object is passed as a parameter
+    f(window.jQuery, window, document);
+
+}(function ($, window, document) {
+
+    // The $ is now locally scoped
+
+    // Listen for the jQuery ready event on the document
+    // https://learn.jquery.com/using-jquery-core/document-ready/
+    $(function () {
+
+        $('#mainNav').affix({
+            offset: {
+                top: 30
+            }
+        });
+
+        console.log('The DOM is ready');
+
+    });
+
+    console.log('The DOM may not be ready');
+
+}));
